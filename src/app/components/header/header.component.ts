df26c33148b5dcf8b778c2223582efcd3775b550
Jayvz Olazo <jayvzolazo@gmail.com>
2018-09-03 17:18:08 +0800

    added search form in navbar, refactor some css codes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+  faSearch
+} from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -8,6 +12,10 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   brand = 'ngular Admin';
+
+  icon = {
+    search: faSearch
+  };
 
   constructor() { }
 
