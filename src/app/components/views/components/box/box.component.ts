7dc41230ce64b79e2a144031b605f4138d10640e
Jayvz Olazo <jayvzolazo@gmail.com>
2018-09-05 11:15:57 +0800

    box updated, styling updated, dashboard updated now using box

--- src/app/components/views/components/box/box.component.ts
+++ src/app/components/views/components/box/box.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoxComponent implements OnInit {
 
+  sampleBoxContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ' +
+    'incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ' +
+    'exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure ' +
+    'dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ' +
+    'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit ' +
+    'anim id est laborum.';
+
+  boxes = [
+    { title: 'Box Title 1', content: this.sampleBoxContent, class: 'warning' },
+    { title: 'Box Title 2', content: this.sampleBoxContent, class: 'danger' },
+    { title: 'Box Title 3', content: this.sampleBoxContent, class: 'success' },
+    { title: 'Box Title 4', content: this.sampleBoxContent, class: 'info' }
+  ];
+
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
